0e561ab9d7368542ed7dac199ec1cc5ab257b962
ras@fedora <ras@fedora>
2026-05-03 14:17:25 +0100

    codecrafters submit [skip ci]

--- app/main.go
+++ app/main.go
@@ -7,17 +7,17 @@ import (
 )
 
 func main() {
-	fmt.Println("Starting TCP server...")
-
 	l, err := net.Listen("tcp", "0.0.0.0:4221")
 	if err != nil {
 		fmt.Println("Failed to bind to port 4221")
 		os.Exit(1)
 	}
 
-	_, err = l.Accept()
+	conn, err := l.Accept()
 	if err != nil {
-		fmt.Println("Error accepting connection: ", err.Error())
+		fmt.Println("Error accepting connection")
 		os.Exit(1)
 	}
+
+	conn.Write([]byte("HTTP/1.1 200 OK\r\n\r\n"))
 }
